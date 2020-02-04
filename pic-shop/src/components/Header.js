9e09983594942dfb63b7de855d15a3640df6d25b
jjyoon7 <jay.yoon7@gmail.com>
2020-02-04 20:46:31 +0100

    fontawesome to remixicon switch

--- pic-shop/src/components/Header.js
+++ pic-shop/src/components/Header.js
@@ -1,19 +1,10 @@
 import React from "react"
-import FontAwesome from 'react-fontawesome'
-import faStyles from 'font-awesome/css/font-awesome.css'
 
 function Header() {
     return (
         <header>
             <h2>Pic Some</h2>
-            <FontAwesome
-              className="super-crazy-colors"
-              name="shopping-cart"
-              cssModule={faStyles}
-              size="2x"
-              spin
-              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-            />
+            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
         </header>
     )
 }
