0780324a24f53bd80eac08e59a2b197f0ccb1039
jjyoon7 <jay.yoon7@gmail.com>
2020-02-04 21:41:32 +0100

    use 'useEffect()' hook to fetch the APi data and replace the photosArr with fetched 'data'

--- pic-shop/src/Context.js
+++ pic-shop/src/Context.js
@@ -1,9 +1,16 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider(props) {
     const [photosArr, setPhotosArr] = useState([])
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(response => response.json())
+            .then(data => setPhotosArr(data))
+    }, []) 
+    
     return (
         <Context.Provider value={{photosArr}}>
             {props.children}
@@ -12,3 +19,4 @@ function ContextProvider(props) {
 }
 
 export {ContextProvider, Context}
+
