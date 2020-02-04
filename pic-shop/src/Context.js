ffe2a7926fd617ed5c48529771b25e6f535b6feb
jjyoon7 <jay.yoon7@gmail.com>
2020-02-04 21:25:01 +0100

    adding state to the Context component, using useState(), and pass that state as a value to the Context.Provider tag

--- pic-shop/src/Context.js
+++ pic-shop/src/Context.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider(props) {
+    const [photosArr, setPhotosArr] = useState([])
     return (
-        <Context.Provider value="">
+        <Context.Provider value={photosArr}>
             {props.children}
         </Context.Provider>
     )
