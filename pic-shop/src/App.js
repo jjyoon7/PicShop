c6c7853c32a96f51da9794c1a6c2b2bd354e8f50
jjyoon7 <jay.yoon7@gmail.com>
2020-02-04 21:06:24 +0100

    No need to wrap Header component with Link component in App.

--- pic-shop/src/App.js
+++ pic-shop/src/App.js
@@ -3,15 +3,13 @@ import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
 
-import {Switch, Link, Route} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
       <div>
-        <Link to="/">
-          <Header/>
-        </Link>
-        
+        <Header/>
+
         <Switch>
           <Route path="/cart">
             <Cart/>
