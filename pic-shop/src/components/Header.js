7bbe35890e6a837cab9c7e31eccab2ddfe8cd149
jjyoon7 <jay.yoon7@gmail.com>
2020-02-04 20:40:37 +0100

    font awesome error update

--- pic-shop/src/components/Header.js
+++ pic-shop/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react"
+import FontAwesome from 'react-fontawesome'
+import faStyles from 'font-awesome/css/font-awesome.css'
 
 function Header() {
     return (
         <header>
             <h2>Pic Some</h2>
-            <i className="fas fa-shopping-cart">he</i>
+            <FontAwesome
+              className="super-crazy-colors"
+              name="shopping-cart"
+              cssModule={faStyles}
+              size="2x"
+              spin
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
         </header>
     )
 }
