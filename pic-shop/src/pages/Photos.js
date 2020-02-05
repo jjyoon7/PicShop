2301f6d49a37dac53a921f2afaf7b0a21ff0cdfa
jjyoon7 <jay.yoon7@gmail.com>
2020-02-05 20:40:20 +0100

    map photo data

--- pic-shop/src/pages/Photos.js
+++ pic-shop/src/pages/Photos.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, {useContext} from "react"
+import {Context} from "../Context"
 
 function Photos() {
+    const {photosArr} = useContext(Context)
+
+    // console.log(photosArr)
+    const photos = photosArr.map(photo => <img key={photo.id} src={photo.url}></img>)
     return (
         <div>
-            Photos
+            {photos}
         </div>
     )
 }
