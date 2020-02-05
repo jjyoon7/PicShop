99a6dd18e4ee7c88223a000eac92c230351d1562
jjyoon7 <jay.yoon7@gmail.com>
2020-02-05 21:27:38 +0100

    render hearIcon and plusIcon based on the 'hovered' state

--- pic-shop/src/components/Image.js
+++ pic-shop/src/components/Image.js
@@ -3,8 +3,8 @@ import React, {useState} from "react";
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
 
-    console.log(hovered)
-
+    const hearIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>
     return (
         <div 
             onMouseEnter={() => setHovered(true)} 
@@ -12,6 +12,8 @@ function Image({className, img}) {
             className={`${className} image-container`}
         >
             <img src={img.url} className="image-grid"/>
+            {hearIcon}
+            {plusIcon}
         </div>
     )
 }
