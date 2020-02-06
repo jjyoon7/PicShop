09d9bd9c0f0417e7de8a48158c3d121ad55b7587
jjyoon7 <jay.yoon7@gmail.com>
2020-02-06 21:35:19 +0100

    update cartIcon based on carItems Array has item or not

--- pic-shop/src/components/Header.js
+++ pic-shop/src/components/Header.js
@@ -3,13 +3,14 @@ import {Link} from "react-router-dom"
 import {Context} from "../Context"
 function Header() {
     const {cartItems} = useContext(Context)
+    const cartClass = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
     return (
         <header>
             <h2>
                 <Link to="/">Pic Some</Link>
             </h2>
             <Link to="/cart">
-                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+                <i className={`${cartClass} ri-fw ri-2x`}></i>
             </Link>
         </header>
     )
